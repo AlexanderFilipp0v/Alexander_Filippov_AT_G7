c5f9a08a9ea7cd7af603d6001dd8484a56edd816
AlexanderFilipp0v <yozhick77@gmail.com>
2023-02-21 21:54:53 +0300

    added methodPicture

--- src/homework/day5/ArrayTask.java
+++ src/homework/day5/ArrayTask.java
@@ -1,4 +1,19 @@
 package homework.day5;
 
 public class ArrayTask {
+
+    public static void main(String[] args) {
+        ArrayTask x = new ArrayTask();
+        //x.methodNSum();
+        x.methodPicture();
+    }
+
+    public void methodPicture() {
+
+        System.out.println("\t  ^__^");
+        System.out.println("\t  (oo)\\\\_______");
+        System.out.println("\t (__)\\ ) \\/\\  \\");
+        System.out.println("\t      ||----w |\\");
+        System.out.println("\t      ||     ||");
+    }
 }
