55bc426b0b11af888942c1334084ad872fbd4632
AlexanderFilipp0v <yozhick77@gmail.com>
2023-02-24 22:25:24 +0300

    Files for bubbles

---
+++ src/project/boxing/Bottle.java
@@ -0,0 +1,14 @@
+package bubbles;
+
+public class Bottle {
+
+    private double volume;
+    private Water water;
+    
+    public void open(SparklingWater sparklingWater) {
+    	sparklingWater.degas();
+    }
+    
+    
+    
+}
---
+++ src/project/liquids/SparklingWater.java
@@ -0,0 +1,22 @@
+package bubbles;
+
+public class SparklingWater extends Water{
+	private Bubble[] bubbles;
+	private boolean isOpened;
+	
+	
+	public void pump(Bubble[] bubbles) {
+		
+		
+	}
+	
+	public void degas() {
+		
+		
+		
+	}
+	
+	
+	
+	
+}
