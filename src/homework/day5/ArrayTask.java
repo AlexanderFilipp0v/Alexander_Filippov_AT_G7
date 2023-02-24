fc44c21866e5d8784de5efd544981e9c2273220c
AlexanderFilipp0v <yozhick77@gmail.com>
2023-02-24 19:01:21 +0300

    Added methodNSum

--- src/homework/day5/ArrayTask.java
+++ src/homework/day5/ArrayTask.java
@@ -6,41 +6,41 @@ public class ArrayTask {
 
     public static void main(String[] args) {
         ArrayTask x = new ArrayTask();
-        //x.methodNSum();
+        x.methodNSum();
         //x.methodMNReverse();
         x.methodMonth();
         x.methodPicture();
     }
-    //public void methodNSum() {
-    //int [] array = {1,2,3,4,5,6,7,8,9};
 
-    //for (int i = 0; i < array.length; i++) {
-    //System.out.print(array[i] + " ");
-    //}
-    //System.out.println();
-    //}
+    public void methodNSum() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int n = 4;
+        int sum = 0;
 
-    //public void methodMNReverse() {
-    //int [] array = {1,2,3,4,5,6,7,8,9};
+        for (int i : array) {
+            if (i % n == 0)
+                sum = sum + i;
+        }
+        {
+            System.out.print(sum);
+        }
+        System.out.println();
+    }
 
-    //for (int i = 0; i < array.length; i++) {
-    //System.out.print(array[i] + " ");
-    //}
-    //System.out.println();
-    //}
+    
 
     public void methodMonth() {
         LocalDate date = LocalDate.now();
         int month = date.getMonthValue();
-        int sum = 0;
+        int summ = 0;
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
-        
+
         for (int i : array) {
             if (i % month == 0)
-                sum = sum + i;
+                summ = summ + i;
 
         }
-        System.out.println(sum);
+        System.out.println(summ);
     }
 
 
