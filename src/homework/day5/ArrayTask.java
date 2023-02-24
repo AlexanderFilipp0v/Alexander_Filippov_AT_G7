0f58a7f012db6f3b6611c47f774590f77a24b3e2
AlexanderFilipp0v <yozhick77@gmail.com>
2023-02-24 23:16:31 +0300

    Added methodMNReverse

--- src/homework/day5/ArrayTask.java
+++ src/homework/day5/ArrayTask.java
@@ -7,47 +7,54 @@ public class ArrayTask {
     public static void main(String[] args) {
         ArrayTask x = new ArrayTask();
         x.methodNSum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3);
-        //x.methodMNReverse(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3);
+        x.methodMNReverse(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3);
         x.methodMonth(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31});
         x.methodPicture();
     }
 
     public void methodNSum(int[] array, int n) {
         int sum = 0;
-
         for (int i : array) {
             if (i % n == 0) {
                 sum = sum + i;
             }
         }
-
         System.out.println(sum);
-
-
+        System.out.println();
     }
 
-    //public void methodMNReverse(int[] array, int n) {
-        //int[] arrayn = new int[array.length];
-        //for (int i : array) {
-            //if (array[i] > n) {
-                //arrayn[i] = array[i];
+    public void methodMNReverse(int[] array, int n) {
+        int secondArrayLength = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > n) {
+                secondArrayLength++;
+            }
+        }
 
-                //System.out.print(arrayn[i]);
+        int[] secondArray = new int[secondArrayLength];
+        int j = secondArrayLength - 1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > n) {
+                secondArray[j] = array[i];
+                System.out.print(secondArray[j] + " ");
+                j--;
+            }
 
+        }
+        System.out.println(" ");
 
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.print(secondArray[i] + " ");
 
-            //}
-
-
-        //}
-        //System.out.println();
-    //}
+        }
+        System.out.println(" ");
+        System.out.println();
+    }
 
     public void methodMonth(int[] array) {
         LocalDate date = LocalDate.now();
         int month = date.getMonthValue();
         int summ = 0;
-
 
         for (int i : array) {
             if (i % month == 0)
@@ -55,9 +62,9 @@ public class ArrayTask {
 
         }
         System.out.println(summ);
+        System.out.println(" ");
     }
-
-
+    
     public void methodPicture() {
 
         System.out.println("\t  ^__^");
