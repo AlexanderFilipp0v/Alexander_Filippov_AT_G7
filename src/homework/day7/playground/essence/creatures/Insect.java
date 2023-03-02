8ad5bd8cf7e82ff970b8fc5360931df70f2e2a08
AlexanderFilipp0v <yozhick77@gmail.com>
2023-03-02 23:44:24 +0300

    Added task 2

--- src/homework/day7/playground/essence/creatures/Insect.java
+++ src/homework/day7/playground/essence/creatures/Insect.java
@@ -2,4 +2,7 @@ package homework.day7.playground.essence.creatures;
 
 public abstract class Insect extends Animal {
 
+    public Insect(int mass, String name) {
+        super(mass, name);
+    }
 }
--- src/homework/day7/playground/essence/creatures/Vertebrata.java
+++ src/homework/day7/playground/essence/creatures/Vertebrata.java
@@ -2,4 +2,11 @@ package homework.day7.playground.essence.creatures;
 
 public abstract class Vertebrata extends Animal {
 
+    public Vertebrata(int mass, String name) {
+        super(mass, name);
+    }
+
+    public void eat(Insect food) {
+        System.out.printf("I am %s and I am eating %s ", name, food.getName());
+    }
 }
