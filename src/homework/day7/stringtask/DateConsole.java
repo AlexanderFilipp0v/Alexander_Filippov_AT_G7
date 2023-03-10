236e3491d952d873bd21b007e63bd0a442d07df8
AlexanderFilipp0v <yozhick77@gmail.com>
2023-03-10 23:09:09 +0300

    Added some tasks from stringtask

--- src/homework/day7/stringtask/DateConsole.java
+++ src/homework/day7/stringtask/DateConsole.java
@@ -6,10 +6,7 @@ import java.util.Date;
 public class DateConsole {
     public void DateConsolen() {
         Date date = new Date();
-        //SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd %2$tB yyyy hh:mm");
-
-
-        //System.out.println("Сейчас на дворе " + formatForDateNow.format(dateNow));
-        System.out.printf("%1$s %2$td %2$tB, %2$tY", "Сейчас на дворе:", date);
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd MMMM, yyyy,  h 'часов' mm 'минут'");
+        System.out.println("Сейчас на дворе " + formatDate.format(date));
     }
 }
---
+++ src/homework/day7/stringtask/StringObjectRunner.java
@@ -0,0 +1,21 @@
+package homework.day7.stringtask;
+
+public class StringObjectRunner {
+
+    public static void main(String[] args) {
+
+        Column ex1 = new Column();
+        ex1.methodColumn("String to column");
+
+        Duplicates ex2 = new Duplicates();
+        ex2.DuplicatesWords("one two three one two four");
+
+        //NumbersInArray ex3 = new NumbersInArray();
+        //ex3.MethodNumbersInArray("2dkhfgb9834y98232db32dbabcb23j");
+
+        DateConsole ex5 = new DateConsole();
+        ex5.DateConsolen();
+    }
+
+}
+
